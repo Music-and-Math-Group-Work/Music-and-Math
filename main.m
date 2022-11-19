b54424069de5349ce58eb509e8991a4e1f2c4dc1
Ziqian Wang <2779671262@qq.com>
2022-11-20 02:12:17 +0800

    feat:finish func printer and file main

--- main.m
+++ main.m
@@ -1,1 +1,11 @@
-% left blank for now.
+notes_file = "./input/notes2.json";
+scale_file = "./input/scale2.json";
+[matrix, scale_r, scale_c] = reader(notes_file, 3);
+% disp(matrix)
+% disp(scale_c)
+% disp(scale_r)
+bars_length = 50;
+main_note = 60;
+timeing = 120;
+meter = 4;
+[chain, nmat] = printer(matrix, scale_c, scale_file, bars_length, main_note, timeing, meter);
--- printer.m
+++ printer.m
@@ -1,67 +1,106 @@
-function [chain, nmat] = printer(filename1, filename2, bars_length, main_note, timeing, delta_beat, duration_beat, meter)
+function [chain, nmat] = printer(P, scale_c, scale_file, bars_length, main_note, timeing, meter)
     %myFun - Description
     %
     % Syntax: output = myFun(input)
     %
     % Long description
     % filename1 = "matrix.json";
-    P = loadjson(filename1);
+    % P = loadjson(filename1);
     % filename2 = "scale.json";
-    scale = loadjson(filename2);
+    scale_s = loadjson(scale_file);
 
-    beats_in_bar = meter / duration_beat;
-    notes = scale + main_note;
+    % beats_in_bar = meter / duration_beat;
+    start_beat = 0;
+
+    notes = scale_s + main_note;
+    main_element = find(scale_s == 0);
+    [scale, target0] = rescale(scale_c, notes, main_element);
+    % scale 是转化为绝对音高的scale，含音长
+    % 第一个音，选主音比较好并占长一拍
     beat_time = 60 / timeing;
     % delta_time = beat_time * delta_beat;
-    duration_time = beat_time * duration_beat;
 
-    target0 = find(scale == 0);
-    % 第一个音，选主音比较好?
-    chain = zeros(1, beats_in_bar * bars_length);
+    chain = zeros(1, meter * bars_length);
     chain(1) = target0;
-    l = size(scale);
-    l = l(2);
+    l = height(scale);
     P_i = zeros(l, 1);
     P_i(target0) = 1;
 
-    nmat = zeros(beats_in_bar * bars_length, 7);
-    nmat(1, :) = [1, duration_beat, 1, main_note, 75, 0, duration_time];
-    beat_now = delta_beat + 1;
+    nmat = zeros(meter * bars_length, 7);
+    nmat(1, :) = [1, 1, 0, main_note, 75, 0, beat_time];
+    beat_now = start_beat + 1;
+    i = 2;
+    j = 2;
+    k = 1;
+    end_beat = meter * bars_length + start_beat;
 
-    for i = 2:bars_length * beats_in_bar
-
-        P_i = P * P_i;
-        P_i = P_i / sum(P_i);
-        prob = cumsum(P_i);
-        choice = rand();
-        temp = find(prob >= choice);
-        target = temp(1);
+    while beat_now <= end_beat
+        [P_i, target] = cal(P, P_i);
         chain(i) = target;
-
-        if mod(i, beats_in_bar) == 1
-            P_i = zeros(l, 1);
-            P_i(target) = 1;
-        end
+        i = i + 1;
+        note = scale(target, 2);
+        duration_beat = scale(target, 1);
 
         % 以上生成链
-        note = notes(target);
-        nmat(i, :) = [beat_now, duration_beat, 0, note, 75, beat_now * beat_time, duration_time];
-        beat_now = beat_now + delta_beat;
-        disp(P_i)
+        if note ~= 0
+            nmat(j, :) = [beat_now, duration_beat, 0, note, 75, beat_now * beat_time, duration_beat * beat_time];
+            j = j + 1;
+        end
 
-        if mod(i, beats_in_bar) == 0 && target == target0
+        beat_now = beat_now + scale(target, 1);
+
+        if mod(beat_now, meter) == start_beat && note == main_note
             break
         end
+
+        if mod(beat_now, 2 * meter) == start_beat || k > 20
+            P_i = zeros(l, 1);
+            P_i(target) = 1;
+            k = 1;
+        else
+            k = k + 1;
+        end
+
+        % disp(P_i)
 
     end
 
     disp(i)
-    nmat = nmat(1:i, :);
+    disp(j)
+    nmat = nmat(1:j, :);
     chain = chain(1, 1:i);
-    % disp(chain)
-    % disp(nmat)
-    writemidi(nmat, "test.mid", timeing);
+    disp(chain)
+    disp(nmat)
+    writemidi(nmat, "./output/output.mid", timeing);
 
+end
+
+function [scale, target0] = rescale(scale_c, notes, main_element)
+    w = height(scale_c);
+    scale = zeros(w, 2);
+
+    for i = 1:w
+        temp = scale_c(i, 2);
+        scale(i, 1) = scale_c(i, 1);
+
+        if temp == 0
+            continue
+        elseif temp == main_element && scale_c(i, 1) == 1
+            target0 = i;
+        end
+
+        scale(i, 2) = notes(temp);
+    end
+
+end
+
+function [P_i, target] = cal(P, P_0)
+    P_i = P * P_0;
+    P_i = P_i / sum(P_i);
+    prob = cumsum(P_i);
+    choice = rand();
+    temp = find(prob >= choice);
+    target = temp(1);
 end
 
 % bars_length = 50;

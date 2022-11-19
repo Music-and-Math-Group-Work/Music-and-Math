3d3336366747dbdbc86024463ad351e3d7c0e2da
pzk23 <pzk23@foxmail.com>
2022-11-19 12:20:41 +0800

    finish reader

--- reader.m
+++ reader.m
@@ -1,73 +1,77 @@
-% these below can all be changed.!!!!!!!!!!!!!
-
-function [matrix, scale] = reader(filename1, filename2, type)
-    % myFun - Description
-    %
-    % Syntax: output = myFun(input)
-    %
-    % @param filename1 二元组json文件路径
-    % @param filename2 音差json文件路径
+function [matrix, scale_r, scale_c] = reader(filename, type)
+    % @brief 给定json和需要的转移矩阵类型，输出转移矩阵和行列的索引
+    % @param filename 二元组json文件路径
     % @param type 需要的转移矩阵类型
     %               1: 音高
     %               2: 时值
     %               3: (时值,音高)
-    %               4: 二阶音高 (unfinished)
+    %               4: 音高-二阶
+    % @return matrix 转移概率矩阵
+    % @return scale_r 行索引
+    % @return scale_c 列索引
+    % 索引说明
+    % 所有音高未按音差文件转义
+    % (时值,音高) 格式同输入
+    % 音高-二阶 行索引为音高二元组，列索引为单一音高
 
-    seq_in = loadjson(filename1);
-    pitch_diff = loadjson(filename2);
+    seq_in = loadjson(filename);
 
     switch type
         case 1
-            matrix = get_transfer(seq_in(:,2), 1);
+            [matrix, scale_r] = get_transfer(seq_in(:, 2));
+            scale_c = scale_r;
         case 2
-            matrix = get_transfer(seq_in(:,1), 1);
+            [matrix, scale_r] = get_transfer(seq_in(:, 1));
+            scale_c = scale_r;
         case 3
             seq_tmp = zeros(height(seq_in), 1);
             for i = 1:height(seq_in)
                 seq_tmp(i) = seq_in(i, 2) * 100 + seq_in(i, 1);
             end
-            matrix = get_transfer(seq_tmp, 1);
-        case 4
-            seq_tmp = zeros(height(seq_in) - 1, 1);
-            for i = 1:height(seq_in)
-                seq_tmp(i) = seq_in(i - 1, 2) * 100 + seq_in(i, 2);
+            [matrix, scale_tmp] = get_transfer(seq_tmp);
+            scale_r = zeros(height(scale_tmp), 2);
+            for i = 1:height(scale_tmp)
+                scale_r(i, 2) = floor(scale_tmp(i) / 100);
+                scale_r(i, 1) = scale_tmp(i) - scale_r(i, 2) * 100;
             end
-            matrix = get_transfer(seq_in(:,2), 2);
+            scale_c = scale_r;
+        case 4
+            arr = seq_in(:, 2);
+            scale_c = unique(arr);
+            scale_r = zeros(height(arr) - 1, 2);
+            for i = 1:height(arr) - 1
+                scale_r(i, 1) = arr(i);
+                scale_r(i, 2) = arr(i + 1);
+            end
+            scale_r = unique(scale_r, "rows");
+            
+            matrix = zeros(height(scale_r), height(scale_c));
+            for k = 3:height(arr)
+                [~, i] = ismember([arr(k - 2), arr(k - 1)], scale_r, "rows");
+                j = find(scale_c == arr(k));
+                matrix(i, j) = matrix(i, j) + 1;
+            end
+            matrix = normalize_line(matrix);
+
         otherwise
             warning('Unexpected type');
     end
-
-    scale = 0; % unfinished
-
 end
 
-function transfer = get_transfer(arr, order)
-    arr_unique = unique(arr);
+function [transfer, state] = get_transfer(arr)
+    state = unique(arr);
 
     len = height(arr);
-    num = height(arr_unique);
-
-    if order == 1
-        transfer = zeros(num);
-        for k = 2:len
-            i = find(arr_unique == arr(k - 1));
-            j = find(arr_unique == arr(k));
-            transfer(i, j) = transfer(i, j) + 1;
-        end
-    elseif order == 2 % unfinished
-        transfer = zeros(num * num, num);
-        for k = 3:len
-            i = (find(arr_unique == arr(k - 2)) - 1) * num + find(arr_unique == arr(k - 1));
-            j = find(arr_unique == arr(k));
-            transfer(i, j) = transfer(i, j) + 1;
-        end
-    else 
-        transfer = 1;
+    num = height(state);
+    
+    transfer = zeros(num);
+    for k = 2:len
+        i = find(state == arr(k - 1));
+        j = find(state == arr(k));
+        transfer(i, j) = transfer(i, j) + 1;
     end
 
-    % size(transfer)
     transfer = normalize_line(transfer);
-    % size(transfer)
 end
 
 function mat_out = normalize_line(mat_in)
@@ -85,6 +89,7 @@ function mat_out = normalize_line(mat_in)
             end
         else
             mat_out(i, 1) = 1;
+            warning(['Sum of row ', num2str(i), ' is 0.']);
         end
     end
 end
